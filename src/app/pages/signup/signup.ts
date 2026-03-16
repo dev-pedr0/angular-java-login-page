1e140b012e1cbcb15db977b540549c1de2bb8907
Pedro Costa <pedro4costa@outlook.com>
2026-03-16 17:54:18 -0300

    feat: created signup page

---
+++ src/app/pages/signup/signup.ts
@@ -0,0 +1,48 @@
+import { Component, inject } from '@angular/core';
+import { DefaultLoginLayout } from '../../components/default-login-layout/default-login-layout';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PrimaryInput } from '../../components/primary-input/primary-input';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login-service';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-signup',
+  imports: [
+    DefaultLoginLayout,
+    ReactiveFormsModule,
+    PrimaryInput,
+  ],
+  providers: [
+    LoginService,
+  ],
+  templateUrl: './signup.html',
+  styleUrl: './signup.scss',
+})
+export class Signup {
+  signupForm!: FormGroup;
+
+  constructor(
+    private router: Router,
+    private loginService: LoginService
+  ){
+    this.signupForm = new FormGroup ({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      passwordConfirm: new FormControl('', [Validators.required, Validators.minLength(6)])
+    })
+  }
+
+  toastr = inject(ToastrService);
+
+  submit(){
+    this.loginService.login(this.signupForm.value.email, this.signupForm.value.password).subscribe({
+      next: () => this.toastr.success("Login feito com sucesso"),
+      error: () => this.toastr.error("Erro Inesperado"),
+    })
+  }
+  navigate(){
+    this.router.navigate(["/login"])
+  }
+}
